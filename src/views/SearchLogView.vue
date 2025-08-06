4f64d0478acdc96b24d293b955072663e25dd7ad
12Non-no <nsuzuki@dl-techx-xedge.net>
2025-08-06 15:08:08 +0900

    資源のデプロイ

--- src/views/SearchLogView.vue
+++ src/views/SearchLogView.vue
@@ -2,7 +2,8 @@
     <v-container>
         <v-card class="mx-auto pt-10 pb-10 rounded-xl" width="1080px" style="background-color: rgba(255, 255, 255, 0.6)">
             <v-row justify="center" class="text-center text-body-2" v-if="weatherData" cols="8" style="white-space: pre-wrap;">
-    {{ weatherData.text }}
+              検索履歴
+    <!-- {{ weatherData.text }} -->
     <br><br>
       情報取得日時：{{ weatherData && weatherData.reportDatetime }}
             </v-row>
